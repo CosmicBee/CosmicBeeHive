d3ccb78b8158652ec617552bececd72fb26ecf45
CosmicBee <bmhcanada@gmail.com>
2022-09-28 01:16:50 -0700

    Layout set up

---
+++ index.js
@@ -0,0 +1,65 @@
+//Crypto wallet alert
+
+function myFunction() {
+    alert("Thank you🙏for the ramen🍜 Wallet: 0x6BD06B139f44cac8652D158567C3F13c772a3bdE  ");
+  }
+
+  function myD() {
+    alert("Thank you your kindness will bring many 🐼bears🐻 a brighter future✨ Wallet: W0xC1bD9881120aDbdE106CB6d1D75dEe7c87EA1137 ");
+  }
+
+
+  function myGame() {
+    alert("Thank you for helping get this community to the next level🎮 Wallet: 0x5B76D4CBF1a3FF4c4Cd201a7a82a4BF6B17c7C9f  ");
+  }
+
+
+
+
+
+
+
+
+
+ //teddy lovebear
+
+
+ function myRear() {
+  alert("You are amazingly talented and will achieve everything you put your mind to. I love you 💖");
+}
+function myLear() {
+  alert(" Look at how far you have come already. You’re stronger than any adversity. You’ve got this.");
+}
+
+function myRleg() {
+  alert("I’m so proud of you and know that you will excel. ");
+}
+
+function myLleg() {
+  alert("You deserve this opportunity. You’re ready for this and you will succeed. ");
+}
+
+function myH() {
+  alert("You've got this. I believe in you.");
+}
+
+
+
+function myRarm() {
+  alert("I love you no matter what. ");
+}
+
+
+function myLarm() {
+  alert(" You are an incredible person.");
+}
+
+function myBod() {
+  alert(" Don't worry. You’ve got this. I love you and have your back.");
+}
+
+
+
+
+
+
